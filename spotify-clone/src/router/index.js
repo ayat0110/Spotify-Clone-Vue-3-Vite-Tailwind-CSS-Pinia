0d70fb437d0e5281fec4f840a0bd8d78d665603e
Ayat AL-ZAIDI <ayatalzidi2000@gmail.com>
2023-04-29 21:58:33 +0300

    added CategorySelect component and search view page

--- spotify-clone/src/router/index.js
+++ spotify-clone/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import SearchView from '../views/SearchView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,7 +9,10 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-  
+    {
+      path: '/search',
+      component: SearchView
+    },
   ]
 })
 
